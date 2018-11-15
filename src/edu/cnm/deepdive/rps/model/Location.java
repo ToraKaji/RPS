ab55012ebffad60ef903774f9da0d61e4c886b28
ToraKaji <ogradysjr@gmail.com>
2018-11-15 10:17:32 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/rps/model/Location.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive.rps.model;
+
+import java.awt.Point;
+
+public class Location {
+
+  private final int row;
+  private final int column;
+
+  public Location(int row, int column) {
+    this.row = row;
+    this.column = column;
+  }
+
+  public int getRow() {
+    return row;
+  }
+
+  public int getColumn() {
+    return column;
+  }
+}
